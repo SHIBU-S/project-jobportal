fb653a1ac8197f3e1a32eb5d8703cd93ab2d1b4b
SHIBU-S <shibusahadevan018@gmail.com>
2024-12-26 17:13:50 +0530

    Project

--- src/AddCategory.jsx
+++ src/AddCategory.jsx
@@ -1,4 +1,121 @@
-import { Container,Row,Col } from "react-bootstrap";
+// import { Container,Row,Col } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { PiGreaterThanLight } from "react-icons/pi";
+// import FloatingLabel from 'react-bootstrap/FloatingLabel';
+// import Form from 'react-bootstrap/Form';
+// import { RiStarSFill } from "react-icons/ri";
+// import { FaPencilAlt } from "react-icons/fa";
+// import { FaRegSave } from "react-icons/fa";
+// import { FcUpLeft } from "react-icons/fc";
+// import { EditorState } from 'draft-js';
+// import { Editor } from 'react-draft-wysiwyg';
+// import { useState } from "react";
+
+
+// function AddCategory({setActiveTab}){
+
+//     const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+//     const onEditorStateChange = (newEditorState) => {
+//         setEditorState(newEditorState);
+//     };
+
+//     function backtocategory(){
+//         setActiveTab("Categories");
+//     }
+
+//     return(
+//         <>
+//             <Container fluid className="adminpagebg">
+//                 <Row className="border px-2">
+//                     <Col className="d-flex align-items-center">
+//                         <h2 className="Categories_title">Categories</h2>
+//                         <ul className="d-flex pt-4 Dashboard_lists">
+//                             <li><Link to="" className="text-decoration-none" style={{color:"grey"}}>Home <PiGreaterThanLight className="p-1"/></Link></li>
+//                             <li><Link to=""  className="text-decoration-none">Categories</Link></li>    
+//                         </ul>    
+//                     </Col>
+
+//                     <Col className="d-flex align-items-center justify-content-end grid gap-2">
+//                         <div className="bg-primary p-2 d-flex"><FaRegSave fill="white"/></div>
+//                         <div className="bg-white p-2 d-flex" onClick={backtocategory}><FcUpLeft/></div>
+//                     </Col>
+//                 </Row>
+//             </Container>
+
+//             <Container fluid className="adminpagebg border px-3">
+//                 <Row>
+//                     <Col>
+//                         <div className="border mt-3" style={{height:"auto"}}>
+//                             <div className="border p-2"><h6 className="d-flex fw-lighter"><FaPencilAlt className="me-2"/> Add Category</h6></div>
+
+//                             <div className="bg-white px-2">
+//                                 <Row className="adminpanelformpage">
+//                                     <Col sm={2} className="d-flex align-items-center"><span className="ms-auto d-flex"><RiStarSFill className="p-1" fill="red"/>Category Name :</span></Col>
+//                                     <Col className="px-4"> 
+//                                         <FloatingLabel controlId="floatingInput" label="Category Name" className="mb-3 mt-3" >
+//                                             <Form.Control type="text" className="p-0" placeholder="Category Name" />
+//                                         </FloatingLabel>
+//                                     </Col>
+//                                 </Row>
+
+//                                 <Row className="adminpanelformpage mt-3 pb-3">
+//                                     <Col sm={2} className="d-flex "><span className="ms-auto d-flex">Description :</span></Col>
+//                                     <Col className="px-4"> 
+//                                         <Editor
+//                                             editorState={editorState}
+//                                             wrapperClassName="demo-wrapper"
+//                                             editorClassName="demo-editor"
+//                                             onEditorStateChange={onEditorStateChange}
+//                                         />
+//                                     </Col>
+//                                 </Row>
+
+//                                 <Row className="adminpanelformpage ">
+//                                     <Col sm={2} className="d-flex align-items-center"><span className="ms-auto">Salary :</span></Col>
+//                                     <Col className="px-4"> 
+//                                         <FloatingLabel controlId="floatingInput" label="Salary" className="mb-3 mt-3" >
+//                                             <Form.Control type="number" className="p-0" placeholder="Salary" />
+//                                         </FloatingLabel>
+//                                     </Col>
+//                                 </Row>
+
+//                                 <Row className="adminpanelformpage ">
+//                                     <Col sm={2} className="d-flex align-items-center"><span className="ms-auto">Location :</span></Col>
+//                                     <Col className="px-4"> 
+//                                         <FloatingLabel controlId="floatingInput" label="Location" className="mb-3 mt-3" >
+//                                             <Form.Control type="text" className="p-0" placeholder="Location" />
+//                                         </FloatingLabel>
+//                                     </Col>
+//                                 </Row>
+
+//                                 <Row className="adminpanelformpage ">
+//                                     <Col sm={2} className="d-flex align-items-center"><span className="ms-auto">Image :</span></Col>
+//                                     <Col className="px-4"> 
+//                                         <FloatingLabel controlId="floatingInput" className="mb-3 mt-3" >
+//                                             <Form.Control type="file" className="py-3" />
+//                                         </FloatingLabel>
+//                                     </Col>
+//                                 </Row>
+
+//                             </div>
+//                         </div>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </>
+//     )
+// }
+
+// export default AddCategory;
+
+
+
+
+
+
+
+import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { PiGreaterThanLight } from "react-icons/pi";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
@@ -9,36 +126,70 @@ import { FaRegSave } from "react-icons/fa";
 import { FcUpLeft } from "react-icons/fc";
 import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+function AddCategory({ setActiveTab }) {
+    const [Categoryname, setcategoryname] = useState("");
+    const [descriptionname, setdescriptionname] = useState("");
+    const [Salary, setsalary] = useState("");
+    const [Locationname, setLocationname] = useState("");
+    const [Image, setImage] = useState(null);
 
-function AddCategory({setActiveTab}){
+    const [displayCategorydatas, setDisplayCategoryDatas] = useState([]);
 
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
     const onEditorStateChange = (newEditorState) => {
         setEditorState(newEditorState);
+        const contentState = newEditorState.getCurrentContent();
+        const description = contentState.getPlainText();
+        setdescriptionname(description);
     };
 
-    function backtocategory(){
+    function backtocategory() {
         setActiveTab("Categories");
     }
 
-    return(
+    function displayCategorydetails() {
+        if (!Categoryname || !descriptionname || !Salary || !Locationname) {
+            alert("Please enter all fields");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("Categoryname", Categoryname);
+        formData.append("CategoryDescription", descriptionname);
+        formData.append("CategorySalary", Salary);
+        formData.append("CategoryLocation", Locationname);
+        // if (Image) {
+            formData.append("CategoryImage", Image);
+        // }
+
+        axios.post("http://localhost:5005/InsertCategoryDatas", formData)
+            .then(() => {
+                alert("Data inserted successfully");
+            })
+            .catch((error) => {
+                alert("Error inserting data: " + error.message);
+            });
+    }
+
+
+    return (
         <>
             <Container fluid className="adminpagebg">
                 <Row className="border px-2">
                     <Col className="d-flex align-items-center">
                         <h2 className="Categories_title">Categories</h2>
                         <ul className="d-flex pt-4 Dashboard_lists">
-                            <li><Link to="" className="text-decoration-none" style={{color:"grey"}}>Home <PiGreaterThanLight className="p-1"/></Link></li>
-                            <li><Link to=""  className="text-decoration-none">Categories</Link></li>    
-                        </ul>    
+                            <li><Link to="" className="text-decoration-none" style={{ color: "grey" }}>Home <PiGreaterThanLight className="p-1" /></Link></li>
+                            <li><Link to="" className="text-decoration-none">Categories</Link></li>
+                        </ul>
                     </Col>
-
                     <Col className="d-flex align-items-center justify-content-end grid gap-2">
-                        <div className="bg-primary p-2 d-flex"><FaRegSave fill="white"/></div>
-                        <div className="bg-white p-2 d-flex" onClick={backtocategory}><FcUpLeft/></div>
+                        <div className="bg-primary p-2 d-flex" onClick={displayCategorydetails}><FaRegSave fill="white" /></div>
+                        <div className="bg-white p-2 d-flex" onClick={backtocategory}><FcUpLeft /></div>
                     </Col>
                 </Row>
             </Container>
@@ -46,22 +197,21 @@ function AddCategory({setActiveTab}){
             <Container fluid className="adminpagebg border px-3">
                 <Row>
                     <Col>
-                        <div className="border mt-3" style={{height:"auto"}}>
-                            <div className="border p-2"><h6 className="d-flex fw-lighter"><FaPencilAlt className="me-2"/> Add Category</h6></div>
-
+                        <div className="border mt-3">
+                            <div className="border p-2"><h6 className="d-flex fw-lighter"><FaPencilAlt className="me-2" /> Add Category</h6></div>
                             <div className="bg-white px-2">
                                 <Row className="adminpanelformpage">
-                                    <Col sm={2} className="d-flex align-items-center"><span className="ms-auto d-flex"><RiStarSFill className="p-1" fill="red"/>Category Name :</span></Col>
-                                    <Col className="px-4"> 
-                                        <FloatingLabel controlId="floatingInput" label="Category Name" className="mb-3 mt-3" >
-                                            <Form.Control type="text" className="p-0" placeholder="Category Name" />
+                                    <Col sm={2} className="d-flex align-items-center"><span className="ms-auto d-flex"><RiStarSFill fill="red" /> Category Name :</span></Col>
+                                    <Col className="px-4">
+                                        <FloatingLabel controlId="floatingInput" label="Category Name" className="mb-3 mt-3">
+                                            <Form.Control type="text" placeholder="Category Name" onChange={(e) => setcategoryname(e.target.value)} />
                                         </FloatingLabel>
                                     </Col>
                                 </Row>
 
                                 <Row className="adminpanelformpage mt-3 pb-3">
                                     <Col sm={2} className="d-flex "><span className="ms-auto d-flex">Description :</span></Col>
-                                    <Col className="px-4"> 
+                                    <Col className="px-4">
                                         <Editor
                                             editorState={editorState}
                                             wrapperClassName="demo-wrapper"
@@ -71,41 +221,39 @@ function AddCategory({setActiveTab}){
                                     </Col>
                                 </Row>
 
-                                <Row className="adminpanelformpage ">
+                                <Row className="adminpanelformpage">
                                     <Col sm={2} className="d-flex align-items-center"><span className="ms-auto">Salary :</span></Col>
-                                    <Col className="px-4"> 
-                                        <FloatingLabel controlId="floatingInput" label="Salary" className="mb-3 mt-3" >
-                                            <Form.Control type="number" className="p-0" placeholder="Salary" />
+                                    <Col className="px-4">
+                                        <FloatingLabel controlId="floatingInput" label="Salary" className="mb-3 mt-3">
+                                            <Form.Control type="number" placeholder="Salary" onChange={(e) => setsalary(e.target.value)} />
                                         </FloatingLabel>
                                     </Col>
                                 </Row>
 
-                                <Row className="adminpanelformpage ">
-                                    <Col sm={2} className="d-flex align-items-center"><span className="ms-auto">Location :</span></Col>
-                                    <Col className="px-4"> 
-                                        <FloatingLabel controlId="floatingInput" label="Location" className="mb-3 mt-3" >
-                                            <Form.Control type="text" className="p-0" placeholder="Location" />
+                                <Row className="adminpanelformpage">
+                                    <Col sm={2}  className="d-flex align-items-center"><span className="ms-auto">Location :</span></Col>
+                                    <Col className="px-4">
+                                        <FloatingLabel controlId="floatingInput" label="Location" className="mb-3 mt-3">
+                                            <Form.Control type="text" placeholder="Location" onChange={(e) => setLocationname(e.target.value)} />
                                         </FloatingLabel>
                                     </Col>
                                 </Row>
 
-                                <Row className="adminpanelformpage ">
-                                    <Col sm={2} className="d-flex align-items-center"><span className="ms-auto">Image :</span></Col>
-                                    <Col className="px-4"> 
-                                        <FloatingLabel controlId="floatingInput" className="mb-3 mt-3" >
-                                            <Form.Control type="file" className="py-3"  />
+                                <Row className="adminpanelformpage">
+                                    <Col sm={2}  className="d-flex align-items-center"><span className="ms-auto">Image :</span></Col>
+                                    <Col className="px-4">
+                                        <FloatingLabel controlId="floatingInput" className="mb-3 mt-3">
+                                            <Form.Control type="file" className="py-3" onChange={(e) => setImage(e.target.files[0])} />
                                         </FloatingLabel>
                                     </Col>
                                 </Row>
-
                             </div>
                         </div>
                     </Col>
                 </Row>
             </Container>
         </>
-    )
+    );
 }
 
 export default AddCategory;
-
--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,70 +1,176 @@
-import { Container,Row,Col } from "react-bootstrap";
+// import { Container,Row,Col } from "react-bootstrap";
+// import { Link, useNavigate } from "react-router-dom";
+// import { FaPlus } from "react-icons/fa";
+// import { FiRefreshCw } from "react-icons/fi";
+// import { RiDeleteBin5Fill } from "react-icons/ri";
+// import { FaPen } from "react-icons/fa6";
+// import { IoListSharp } from "react-icons/io5";
+// import { PiGreaterThanLight } from "react-icons/pi";
+// import { useState } from "react";
+
+
+// function Categories({setActiveTab}){
+
+//     const [showAddCategory,setshowAddCategory] = useState(false);
+
+//     function displaynew(){
+//         setshowAddCategory(true);
+//         setActiveTab('AddCategory');
+//     }
+
+//     return(
+//         <>
+//             <Container fluid  className="adminpagebg ">
+//                 <Row className="px-2">
+//                     <Col className="d-flex align-items-center">
+//                         <h2 className="Categories_title">Categories</h2>
+//                         <ul className="d-flex pt-4 Dashboard_lists">
+//                             <li><Link to="" className="text-decoration-none" style={{color:"grey"}}>Home <PiGreaterThanLight className="p-1"/></Link></li>
+//                             <li><Link to=""  className="text-decoration-none">Categories</Link></li>    
+//                         </ul>    
+//                     </Col>
+
+//                     <Col className=" d-flex align-items-center grid gap-2">
+//                         <div className="ms-auto p-2 d-flex bg-primary" onClick={displaynew}><FaPlus fill="white"/></div> 
+//                         <div className="border p-2 d-flex bg-white"><FiRefreshCw/></div>
+//                         <div className="border p-2 d-flex bg-danger"><RiDeleteBin5Fill fill="white"/></div>
+//                     </Col>
+//                 </Row>
+//             </Container>
+
+//             <Container fluid className="adminpagebg border px-3">
+//                 <div className="border p-2 mt-3 pb-0"><h6 className="Categories_title p-1"><IoListSharp/> Category List</h6></div>
+
+//                 <div className="border p-3 bg-white" style={{height:"500px"}}>
+//                     <table style={{width:"100%"}} className="border">
+//                         <thead>
+//                             <tr>
+//                                 <td className="border p-2" style={{width:"1px"}}><input type="checkbox"></input></td>
+//                                 <td className="border p-2" style={{width:"55%"}}><Link className="text-decoration-none">Category Name</Link></td>
+//                                 <td className="border p-2 text-end"><Link className="text-decoration-none">Sort Order</Link></td>
+//                                 <td className="border p-2 text-end">Action</td>
+//                             </tr>
+//                         </thead>
+
+//                         <tbody>
+//                             <tr>
+//                                 <td className="border p-2 py-3" style={{width:"1px"}}><input type="checkbox"></input></td>
+//                                 <td className="border p-2" style={{width:"55%"}}></td>
+//                                 <td className="border p-2 text-end"></td>
+//                                 <td className="p-2 d-flex mt-1"><div className="ms-auto p-2 d-flex align-items-center bg-primary"><FaPen fill="white"/></div></td>
+//                             </tr>
+//                         </tbody>
+//                     </table>
+//                 </div>
+//             </Container>
+//         </>
+//     )
+// }
+
+// export default Categories;
+
+
+
+
+
+import { Container, Row, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { FaPlus } from "react-icons/fa";
 import { FiRefreshCw } from "react-icons/fi";
 import { RiDeleteBin5Fill } from "react-icons/ri";
-import { FaPen } from "react-icons/fa6";
+import { FaPen } from "react-icons/fa";
 import { IoListSharp } from "react-icons/io5";
 import { PiGreaterThanLight } from "react-icons/pi";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Categories({ setActiveTab }) {
+    const [showAddCategory, setshowAddCategory] = useState(false);
+    const [categoryData, setCategoryData] = useState([]); 
 
 
-function Categories({setActiveTab}){
+    useEffect(() => {
+        async function fetchCategoryData() {
+            try {
+                const response = await axios.get("http://localhost:5005/GetCategoryDatas"); 
+                setCategoryData(response.data.totaldatas);  
+            } catch (err) {
+                console.error("Error fetching category data:", err);
+            }
+        }
+        fetchCategoryData(); 
+    }, []); 
 
-    const [showAddCategory,setshowAddCategory] = useState(false);
-
-    function displaynew(){
+    function displaynew() {
         setshowAddCategory(true);
         setActiveTab('AddCategory');
     }
 
-    return(
+    return (
         <>
-            <Container fluid  className="adminpagebg ">
+            <Container fluid className="adminpagebg">
                 <Row className="px-2">
                     <Col className="d-flex align-items-center">
                         <h2 className="Categories_title">Categories</h2>
                         <ul className="d-flex pt-4 Dashboard_lists">
-                            <li><Link to="" className="text-decoration-none" style={{color:"grey"}}>Home <PiGreaterThanLight className="p-1"/></Link></li>
-                            <li><Link to=""  className="text-decoration-none">Categories</Link></li>    
-                        </ul>    
+                            <li><Link to="" className="text-decoration-none" style={{ color: "grey" }}>Home <PiGreaterThanLight className="p-1" /></Link></li>
+                            <li><Link to="" className="text-decoration-none">Categories</Link></li>
+                        </ul>
                     </Col>
 
-                    <Col className=" d-flex align-items-center grid gap-2">
-                        <div className="ms-auto p-2 d-flex bg-primary" onClick={displaynew}><FaPlus fill="white"/></div> 
-                        <div className="border p-2 d-flex bg-white"><FiRefreshCw/></div>
-                        <div className="border p-2 d-flex bg-danger"><RiDeleteBin5Fill fill="white"/></div>
+                    <Col className="d-flex align-items-center grid gap-2">
+                        <div className="ms-auto p-2 d-flex bg-primary" onClick={displaynew}><FaPlus fill="white" /></div>
+                        <div className="border p-2 d-flex bg-white"><FiRefreshCw /></div>
+                        <div className="border p-2 d-flex bg-danger"><RiDeleteBin5Fill fill="white" /></div>
                     </Col>
                 </Row>
             </Container>
 
             <Container fluid className="adminpagebg border px-3">
-                <div className="border p-2 mt-3 pb-0"><h6 className="Categories_title p-1"><IoListSharp/> Category List</h6></div>
+                <div className="border p-2 mt-3 pb-0">
+                    <h6 className="Categories_title p-1"><IoListSharp /> Category List</h6>
+                </div>
 
-                <div className="border p-3 bg-white" style={{height:"500px"}}>
-                    <table style={{width:"100%"}} className="border">
+                <div className="border p-3 bg-white" style={{ height: "500px", overflowY: "auto" }}>
+                    <table style={{ width: "100%" }} className="border">
                         <thead>
                             <tr>
-                                <td className="border p-2" style={{width:"1px"}}><input type="checkbox"></input></td>
-                                <td className="border p-2" style={{width:"55%"}}><Link className="text-decoration-none">Category Name</Link></td>
+                                <td className="border p-2" style={{ width: "1px" }}><input type="checkbox" /></td>
+                                <td className="border p-2" style={{ width: "55%" }}><Link className="text-decoration-none">Category Name</Link></td>
                                 <td className="border p-2 text-end"><Link className="text-decoration-none">Sort Order</Link></td>
                                 <td className="border p-2 text-end">Action</td>
                             </tr>
                         </thead>
 
                         <tbody>
-                            <tr>
-                                <td className="border p-2 py-3" style={{width:"1px"}}><input type="checkbox"></input></td>
-                                <td className="border p-2" style={{width:"55%"}}></td>
-                                <td className="border p-2 text-end"></td>
-                                <td className="p-2 d-flex mt-1"><div className="ms-auto p-2 d-flex align-items-center bg-primary"><FaPen fill="white"/></div></td>
-                            </tr>
+                            {categoryData.length > 0 ? (
+                                categoryData.map((category) => (
+                                    <tr>
+                                        <td className="border p-2 py-3" style={{ width: "1px" }}>
+                                            <input type="checkbox" />
+                                        </td>
+                                        <td className="border p-2" style={{ width: "55%" }}>
+                                            <Link to="" className="text-decoration-none text-dark">{category.Categoryname}</Link>
+                                        </td>
+                                        <td className="border p-2 text-end"></td>
+                                        <td className="border p-2 d-flex ">
+                                            <div className="ms-auto p-2 m-1 d-flex align-items-center bg-primary">
+                                                <FaPen fill="white" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="4" className="text-center"></td>
+                                </tr>
+                            )}
                         </tbody>
                     </table>
                 </div>
             </Container>
         </>
-    )
+    );
 }
 
 export default Categories;
